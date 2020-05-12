facdb12128d22f3f3277117e91154a782f0c6bbd
paulmrest <paulmrest@gmail.com>
2020-05-12 16:46:30 -0700

    Pre-Lab12 Refactoring
    Various refactorings. Substantial code clean up from yesterday.
    
    Also added the functionality that images will not be repeated from
    the previous round (which is probably part of Lab 12).

--- JS/app.js
+++ JS/app.js
@@ -2,80 +2,83 @@
 
 //global variables
 const rootElement = document.getElementById('product-images');
+const imageRootFolder = 'IMG/';
 //static for now...guessing these will be variables later
-const numberOfRounds = 25;
+const numberOfRounds = 5;
 const numberOfImagesPerRound = 3;
 
 var numberOfRoundsPlayed = 0;
 
 var allProductImages = new Array();
 
-var renderedProductImages = new Array();
+var currentlyRenderedImages = new Array();
 
-function ProductImage(productName, filePath) {
+//ProductImage
+function ProductImage(productName, fileName, extension) {
   this.productName = productName;
-  this.filePath = filePath;
+  this.filePath = ProductImage.buildFilePath(fileName, extension);
+  this.altText = productName.replace(' ', '-');
   this.votes = 0;
   this.views = 0;
   allProductImages.push(this);
 }
 
-//class method
-ProductImage.renderRandomImages = function(numberOfImages) {
-  //clear all images under rootElement
-  while (rootElement.firstChild)
-  {
-    rootElement.removeChild(rootElement.firstChild);
-  }
+ProductImage.prototype.renderImage = function() {
   rootElement.style.display = 'flex';
+  var newImgEl = document.createElement('img');
+  //set HTML attributes
+  newImgEl.setAttribute('src', this.filePath);
+  newImgEl.setAttribute('alt', this.altText);
+  newImgEl.setAttribute('title', this.altText);
+  //set CSS styling
+  newImgEl.style.float = 'left';
+  newImgEl.style.margin = 'auto';
+  newImgEl.style.maxWidth = '30%';
+  currentlyRenderedImages.push(this);
+  this.views++;
+  rootElement.appendChild(newImgEl);
+};
+
+//class methods
+ProductImage.buildFilePath = function(fileName, extension) {
+  return `${imageRootFolder}${fileName}.${extension.toLowerCase()}`;
+};
+
+ProductImage.renderRandomImages = function(numberOfImages) {
+  //check if there enough images insstantiated to render
   if (numberOfImages > allProductImages.length)
   {
-    alert('Not enough products to render');
+    console.error('Not enough images to render.');
   }
   else
   {
-    renderedProductImages = new Array();
-    //make a copy of allProductImages array
-    //Array.slice() will copy all object references to the new array
-    var tempProductImagesBuilderArray = allProductImages.slice();
-    for (var i = 0; i < numberOfImages; i++)
+    //clear children of HTML root element
+    while (rootElement.firstChild)
     {
-      var randIndex = getRandMinMaxInclusive(0, tempProductImagesBuilderArray.length - 1);
-      /*
-      Initially the next line used Array.splice(), specifically:
-      var newImageToRender = tempProductImagesBuilderArray.spliace(randIndex, 1);
-      but while the console showed newImageToRender as being assinged to the
-      ProductImage object we expect, and that ProductImage object having all its properities,
-      when we tried to access the properities, they gave "undefined"
-      No...idea...why..???
-
-      Instead I'm just using bracket notation and using splice() at the end, returning that removed
-      element to nothing.
-      */
-      var newImageToRender = tempProductImagesBuilderArray[randIndex];
-      var newImgEl = document.createElement('img');
-      //set HTML attributes
-      newImgEl.setAttribute('src', newImageToRender.filePath);
-      newImgEl.setAttribute('alt', newImageToRender.productName);
-      newImgEl.setAttribute('title', newImageToRender.productName);
-      //set CSS styling
-      newImgEl.style.float = 'left';
-      newImgEl.style.margin = 'auto';
-      newImgEl.style.maxWidth = '30%';
-      renderedProductImages.push(newImageToRender);
-      newImageToRender.views++;
-      rootElement.appendChild(newImgEl);
-      tempProductImagesBuilderArray.splice(randIndex, 1);
+      rootElement.removeChild(rootElement.firstChild);
+    }
+    if (numberOfRoundsPlayed >= numberOfRounds)
+    {
+      rootElement.removeEventListener('click', handleImageClick);
+      ProductImage.displayVotingResults();
+    }
+    else
+    {
+      var tempProductImagesBuilderArray = ProductImage.allProductImagesExceptLastRound();
+      currentlyRenderedImages = new Array();
+      for (var i = 0; i < numberOfImages; i++)
+      {
+        var randIndex = getRandMinMaxInclusive(0, tempProductImagesBuilderArray.length - 1);
+        var newImageToRender = tempProductImagesBuilderArray[randIndex];
+        newImageToRender.renderImage();
+        //remove image from temp array
+        tempProductImagesBuilderArray.splice(randIndex, 1);
+      }
     }
   }
 };
 
-function displayVotingResults() {
-  //remove images to display results
-  while (rootElement.firstChild)
-  {
-    rootElement.removeChild(rootElement.firstChild);
-  }
+ProductImage.displayVotingResults = function() {
   rootElement.style.display = '';
   for (var i = 0; i < allProductImages.length; i++)
   {
@@ -84,26 +87,47 @@ function displayVotingResults() {
     rootElement.appendChild(newParaEl);
     rootElement.appendChild(document.createElement('br'));
   }
-}
+};
 
-function handleImageClick(event) {
-  var clickedImageTitle = event.target.title;
-  for (var i = 0; i < renderedProductImages.length; i++)
+//returns an array of all the ProductImage objects that are in allProductImages but
+//not in currentlyRenderedImages
+ProductImage.allProductImagesExceptLastRound = function() {
+  if (currentlyRenderedImages.length === 0)
   {
-    if (renderedProductImages[i].productName === clickedImageTitle)
+    return allProductImages.slice();
+  }
+  var imagesNotRenderedLastRound = new Array();
+  for (var i = 0; i < allProductImages.length; i++)
+  {
+    var oneProductImage = allProductImages[i];
+    var renderedLastRound = false;
+    for (var j = 0; j < currentlyRenderedImages.length; j++)
     {
-      renderedProductImages[i].votes++;
-      numberOfRoundsPlayed++;
-      if (numberOfRoundsPlayed < numberOfRounds)
+      if (oneProductImage.altText === currentlyRenderedImages[j].altText)
       {
-        ProductImage.renderRandomImages(numberOfImagesPerRound);
+        renderedLastRound = true;
         break;
       }
-      else
+      else if (j >= currentlyRenderedImages.length - 1 && !renderedLastRound)
       {
-        rootElement.removeEventListener('click', handleImageClick);
-        displayVotingResults();
+        imagesNotRenderedLastRound.push(oneProductImage);
       }
+    }
+  }
+  return imagesNotRenderedLastRound;
+};
+
+//non-class functions
+function handleImageClick(event) {
+  var clickedImageHTMLTitle = event.target.title;
+  for (var i = 0; i < currentlyRenderedImages.length; i++)
+  {
+    if (currentlyRenderedImages[i].altText === clickedImageHTMLTitle)
+    {
+      currentlyRenderedImages[i].votes++;
+      numberOfRoundsPlayed++;
+      ProductImage.renderRandomImages(numberOfImagesPerRound);
+      break;
     }
   }
 }
@@ -118,26 +142,26 @@ function getRandMinMaxInclusive(min, max) {
 }
 
 //executables
-new ProductImage('R2D2-Bag', 'IMG/bag.jpg');
-new ProductImage('Banana-Slicer', 'IMG/banana.jpg');
-new ProductImage('Tablet-TP-Stand', 'IMG/bathroom.jpg');
-new ProductImage('Toeless-Bootes', 'IMG/boots.jpg');
-new ProductImage('All-in-One-Breakfast', 'IMG/breakfast.jpg');
-new ProductImage('Meatball-Bubblegum', 'IMG/bubblegum.jpg');
-new ProductImage('Unsittable-Chair', 'IMG/chair.jpg');
-new ProductImage('Cthulhu', 'IMG/cthulhu.jpg');
-new ProductImage('Duck-Mask-For-Dogs', 'IMG/dog-duck.jpg');
-new ProductImage('Dragon-Meat', 'IMG/dragon.jpg');
-new ProductImage('Pen-Cutlery', 'IMG/pen.jpg');
-new ProductImage('Pet-Sweep', 'IMG/pet-sweep.jpg');
-new ProductImage('Pizza-Scissors', 'IMG/scissors.jpg');
-new ProductImage('Shark-Sleeping-Bag', 'IMG/shark.jpg');
-new ProductImage('Baby-Sweep', 'IMG/sweep.png');
-new ProductImage('Tauntaun-Sleeping-Bag', 'IMG/tauntaun.jpg');
-new ProductImage('Unicorn-Meat', 'IMG/unicorn.jpg');
-new ProductImage('Tentacle-USB', 'IMG/usb.gif');
-new ProductImage('Self-Watering-Can', 'IMG/water-can.jpg');
-new ProductImage('Misshapen-Wine-Glass', 'IMG/wine-glass.jpg');
+new ProductImage('R2D2 Bag', 'bag', 'jpg');
+new ProductImage('Banana Slicer', 'banana', 'jpg');
+new ProductImage('Tablet TP Stand', 'bathroom', 'jpg');
+new ProductImage('Toeless Bootes', 'boots', 'jpg');
+new ProductImage('All in One Breakfast', 'breakfast', 'jpg');
+new ProductImage('Meatball Bubblegum', 'bubblegum', 'jpg');
+new ProductImage('Unsittable Chair', 'chair', 'jpg');
+new ProductImage('Cthulhu', 'cthulhu', 'jpg');
+new ProductImage('Duck Mask for Dogs', 'dog-duck', 'jpg');
+new ProductImage('Dragon Meat', 'dragon', 'jpg');
+new ProductImage('Pen Cutlery', 'pen', 'jpg');
+new ProductImage('Pet Sweep', 'pet-sweep', 'jpg');
+new ProductImage('Pizza Scissors', 'scissors', 'jpg');
+new ProductImage('Shark Sleeping Bag', 'shark', 'jpg');
+new ProductImage('Baby Sweep', 'sweep', 'png');
+new ProductImage('Tauntaun Sleeping-Bag', 'tauntaun', 'jpg');
+new ProductImage('Unicorn Meat', 'unicorn', 'jpg');
+new ProductImage('Tentacle USB', 'usb', 'gif');
+new ProductImage('Self Watering Can', 'water-can', 'jpg');
+new ProductImage('Misshapen Wine Glass', 'wine-glass', 'jpg');
 
 setupEventListeners();
 
